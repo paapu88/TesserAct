288dfe5c2b105ee976aecc68c4b0fcb72d7f02b1
paapu68 <markus.kaukonen@iki.fi>
2017-04-10 10:47:01 +0300

    noQTpicture2rekkariPosAndNeg.py toimii

--- noQTpicture2rekkariPosAndNeg.py
+++ noQTpicture2rekkariPosAndNeg.py
@@ -39,14 +39,20 @@ class MouseRectangle():
         self.image = image
 
     def set_init_position(self):
-        upleft = [int(self.image.shape[1] / 2), int(self.image.shape[0] / 2)]
-        downright = [int(self.image.shape[1] / 2) + self.xpixel, int(self.image.shape[0] / 2) + self.ypixel]
-        self.set_refPts([upleft, downright])
+        """ in case there are no mouse points, create ones"""
+        if len(self.refPts) == 0:
+            upleft = [int(self.image.shape[1] / 2), int(self.image.shape[0] / 2)]
+            downright = [int(self.image.shape[1] / 2) + self.xpixel, int(self.image.shape[0] / 2) + self.ypixel]
+            self.set_refPts([upleft, downright])
 
-    def reset(self):
+    def reset(self, pos1=None, pos2=None):
         self.refPts = []
+        self.oldPts = []
         self.cropping = False
-        self.set_init_position()
+        if pos1 is None:
+            self.set_init_position()
+        else:
+            self.set_refPts([pos1, pos2])
 
     def resetToPrevious(self):
         self.set_refPts([self.refPts[0], self.refPts[1]])
@@ -95,8 +101,8 @@ class MoveRectangle():
             ypixel = int(sys.argv[3])
         except:
             print("setting default values for pixels")
-            xpixel = 80
-            ypixel = 20
+            xpixel = 160
+            ypixel = 40
         print("INIT:", xpixel, ypixel)
 
         self.mouse = MouseRectangle(xpixel=xpixel, ypixel=ypixel)
@@ -132,18 +138,89 @@ class MoveRectangle():
             basename = initBasename + str(i) + '.tif'
             if not(os.path.isfile(basename)):
                 break
-        fullname = self.getNewName(oldname=basename, subdir='output')
+        fullname = self.getNewName(oldname=basename, subdir='Positives')
         refPts = self.mouse.get_refPts()
         positive_area = clone[refPts[0][1]:refPts[1][1], refPts[0][0]:refPts[1][0]]
         print("SAVING IMAGE: ", fullname)
         cv2.imwrite(fullname, positive_area)
+        # save details of the rectangle to a text file
+        allname=self.getNewName(oldname='all.box', subdir='Positives')
+        with open(allname, 'a') as f:
+            f.write(basename + \
+                    ' ' + str(refPts[0][0]) + \
+                    ' ' + str(image.shape[1]-refPts[0][1]) + \
+                    ' ' + str(refPts[1][0]) + \
+                    ' ' + str(image.shape[1]-refPts[1][1]) + \
+                    ' 0 \n')
+
+    def saveNegativeImages(self, image, initBasename='negative-', nNegatives=30):
+        """from an image where the positive image was taken, take also nNegative
+        samples which do not overlap with the positive sample"""
+        import random
+        import math
+        random.seed()
+        dx = image.shape[1]
+        dy = image.shape[0]
+        print("dx,dy", dx, dy)
+        for nNegative in range(nNegatives):
+            refPts = self.mouse.get_refPts()
+            (x1,y1) = refPts[0]
+            (x2,y2) = refPts[1]
+            print("x1,y1", x1, y1)
+            print("x2,y2", x2, y2)
+            w=x2-x1
+            h=y2-y1
+            x1_init = x1; y1_init = y1
+            x2_init = x2; y2_init = y2
+            repeat = True
+            while repeat:
+                #random translation
+                movex = round(int(random.randrange(0, dx)))
+                movey = round(int(random.randrange(0, dy)))
+                print("movex, movey", movex, movey, x1, x1_init, w)
+                x1 = x1_init + movex
+                if (x1+w) > dx:
+                    x1 = x1 - dx + w
+                    print("x1,w,dx",x1,w,dx)
+                y1 = y1_init + movey
+                if (y1+h) > dy:
+                    y1 = y1 - dy + h
+                #90 deg rotation by 1/2 change
+                if random.random() > 0.5:
+                    print ("rotated")
+                    x2 = x1 + h
+                    if x2 > dx:
+                        x1 = x1 - dx + h; x2 = x2 - dx + h
+                    y2 = y1 + w
+                    if y2 > dy:
+                        y1 = y1 - dy + w; y2 = y2 - dy + w
+                else:
+                    print("NOT rotated")
+                    x2 = x1 + w
+                    y2 = y1 + h
+
+                # check overlap with the initial rectangle
+                if not((x1 < x1_init < x2) or (x1 < x2_init < x2) \
+                    and \
+                    (y1 < y1_init < y2) or (y1 < y2_init < y2)):
+                    print("accepted x1,y1,x2,y2",x1,y1,x2,y2)
+                    repeat = False
+            negative_area = image.copy()[y1:y2, x1:x2]
+            basename = initBasename + str(nNegative) + '.tif'
+            fullname = self.getNewName(oldname=basename, subdir='Negatives')
+            print("SAVING IMAGE: ", fullname)
+            cv2.imwrite(fullname, negative_area)
+
+
+
+
 
 
     def showDialog(self):
         import math
 
 
-
+        print("FILES:", self.fnames)
         for fname in self.fnames:
                 img = cv2.imread(fname)
                 gray = cv2.imread(fname, 0)
@@ -152,6 +229,7 @@ class MoveRectangle():
                 clone = img.copy()
                 self.mouse.set_image(image=img)
                 self.mouse.set_init_position()
+
                 refPts = self.mouse.get_refPts()
 
                 for i in range(0, len(refPts), 2):
@@ -163,110 +241,113 @@ class MoveRectangle():
 
                 # keep looping until the '*' key is pressed
                 while True:
-                    while True:
-                        # display the image and wait for a keypress
-                        cv2.imshow("image", img)
-                        key = cv2.waitKey(33)
-                        change = False
-                        # if the 'r' key is pressed, reset the cropping region
-                        if key == ord("r"):
-                            img = clone.copy()
-                            self.mouse.reset()
-                            self.mouse.set_image(image=img)
+                    # display the image and wait for a keypress
+                    cv2.imshow("image", img)
+                    key = cv2.waitKey(33)
+                    change = False
+                    # if the 'r' key is pressed, reset the cropping region
+                    if key == ord("r"):
+                        img = clone.copy()
+                        self.mouse.reset()
+                        self.mouse.set_image(image=img)
 
-                        elif key == ord("i"):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][1]= refPts[-2][1] - 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("o"):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][1]= refPts[-2][1] + 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord(","):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-1][1]= refPts[-1][1] + 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("."):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-1][1]= refPts[-1][1] - 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("h"):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][0] = refPts[-2][0] - 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("j"):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][0] = refPts[-2][0] + 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("k"):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-1][0] = refPts[-1][0] - 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("l"):
-                            refPts = self.mouse.get_refPts()
-                            refPts[-1][0] = refPts[-1][0] + 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
+                    elif key == ord("i"):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][1]= refPts[-2][1] - 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("o"):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][1]= refPts[-2][1] + 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord(","):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-1][1]= refPts[-1][1] + 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("."):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-1][1]= refPts[-1][1] - 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("h"):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][0] = refPts[-2][0] - 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("j"):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][0] = refPts[-2][0] + 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("k"):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-1][0] = refPts[-1][0] - 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("l"):
+                        refPts = self.mouse.get_refPts()
+                        refPts[-1][0] = refPts[-1][0] + 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
 
-                        elif key == ord("w"):
-                            #up
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][1] = refPts[-2][1] - 1
-                            refPts[-1][1] = refPts[-1][1] - 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("z"):
-                            #up
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][1] = refPts[-2][1] + 1
-                            refPts[-1][1] = refPts[-1][1] + 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("d"):
-                            #up
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][0] = refPts[-2][0] + 1
-                            refPts[-1][0] = refPts[-1][0] + 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
-                        elif key == ord("a"):
-                            #up
-                            refPts = self.mouse.get_refPts()
-                            refPts[-2][0] = refPts[-2][0] - 1
-                            refPts[-1][0] = refPts[-1][0] - 1
-                            self.mouse.set_refPts(refPts)
-                            change = True
+                    elif key == ord("w"):
+                        #up
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][1] = refPts[-2][1] - 1
+                        refPts[-1][1] = refPts[-1][1] - 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("z"):
+                        #up
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][1] = refPts[-2][1] + 1
+                        refPts[-1][1] = refPts[-1][1] + 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("d"):
+                        #up
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][0] = refPts[-2][0] + 1
+                        refPts[-1][0] = refPts[-1][0] + 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
+                    elif key == ord("a"):
+                        #up
+                        refPts = self.mouse.get_refPts()
+                        refPts[-2][0] = refPts[-2][0] - 1
+                        refPts[-1][0] = refPts[-1][0] - 1
+                        self.mouse.set_refPts(refPts)
+                        change = True
 
-                        # if the '7' key is pressed, break from the loop
-                        elif key == ord("7"):
-                            break
-
-                        # next image
-                        elif key == ord("*"):
-                            break
-                        if change:
-                            change = False
-                            img = clone.copy()
-                            for i in range(0,len(refPts),2):
-                                cv2.rectangle(img, tuple(refPts[i]), tuple(refPts[i+1]), (255, 0, 0), 2)
-                            oldPts = self.mouse.get_oldPts()
-                            for i in range(0, len(oldPts), 2):
-                                cv2.rectangle(img, tuple(oldPts[i]), tuple(oldPts[i + 1]), (0, 255, 0), 1)
-                            self.mouse.set_image(image=img)
-                            
-                    if key == ord("*"):  # if star key was pressed
-                        self.mouse.resetToPrevious()
+                    # if the '7' key is pressed, break from the loop
+                    elif key == ord("7"):
                         break
-                    self.savePositiveImage(gray.copy(), initBasename=fname+'-positive-')
-                    self.mouse.set_oldPts()
-                    #self.mouse.reset()
+
+                    # next image
+                    elif key == ord("*"):
+                        break
+                    if change:
+                        change = False
+                        img = clone.copy()
+                        for i in range(0,len(refPts),2):
+                            cv2.rectangle(img, tuple(refPts[i]), tuple(refPts[i+1]), (255, 0, 0), 2)
+                        oldPts = self.mouse.get_oldPts()
+                        for i in range(0, len(oldPts), 2):
+                            cv2.rectangle(img, tuple(oldPts[i]), tuple(oldPts[i + 1]), (0, 255, 0), 1)
+                        self.mouse.set_image(image=img)
+                            
+                if key == ord("*"):  # if star key was pressed
+                    self.mouse.resetToPrevious()
+                    break
+                self.savePositiveImage(gray.copy(), initBasename=fname+'-positive-')
+                self.saveNegativeImages(gray.copy(), initBasename=fname+'-negative-')
+                pos1 = self.mouse.get_refPts()[-2]
+                pos2 = self.mouse.get_refPts()[-1]
+                self.mouse.reset(pos1=pos1, pos2=pos2)
+                #self.mouse.resetToPrevious()
+                #self.mouse.reset()
 
 
 
